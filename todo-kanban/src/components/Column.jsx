7204f5f5493477e6f0eb5e6a27d77ccdc9f65afe
PawelC <pawel.cacko@wecode.agency>
2026-04-14 16:47:05 +0200

    add starter layout

---
+++ todo-kanban/src/components/Column.jsx
@@ -0,0 +1,11 @@
+export default function Column({ title }) {
+  return (
+    <div className="column">
+      <h3>{title}</h3>
+
+      <div className="column-content">
+        {/* tu będą taski */}
+      </div>
+    </div>
+  );
+}
